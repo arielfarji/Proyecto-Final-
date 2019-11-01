887ea12428ff80128ec09a962fbdc2d70ebd275b
Tiziana Borghetti <tborghetti@udesa.edu.ar>
2019-11-01 10:35:15 -0300

    El buscador ya esta hecho

--- home.js
+++ home.js
@@ -1,18 +1,18 @@
 window.onload = function () {
-var columnas = document.querySelectorAll("div.uk-slider").innerHTML = function() {
-  for (var i = 0; i < columna.length; i++) {
-    columnas.style =
-  }
-};
 
-//modificar el click en la lupa
-.onClick = function buscadorsecundario() {
-  var search = document.querySelector("button.fa fas-search");
-  if (search.style.display === "none") {
-    search.style.display = "block";
-  } else {
-    x.style.display = "none";
+  // var columnas = document.querySelectorAll("div.uk-slider").innerHTML = function() {
+  //   for (var i = 0; i < columna.length; i++) {
+  //     columnas.style =
+  //   }
+  // };
+
+  //modificar el click en la lupa para q aparezca lo oculto
+  var lupita = document.querySelector("#lupita");
+  var inputBuscador = document.querySelector(".buscadorsecundario");
+
+  lupita.onclick = function() {
+    inputBuscador.classList.toggle('inputHidden');
   }
-}
-//hasta aca.
+
+  //hasta aca.
 }
