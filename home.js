a51042a481193492b6c2450eee8df676bc284d78
ariel <afarji@udesa.edu.ar>
2019-10-31 23:14:59 -0300

    Comentarios para modificar lupa
    Agregue un boton en la lupa para tratar de que funcione el buscador

--- home.js
+++ home.js
@@ -1,5 +1,18 @@
+window.onload = function () {
 var columnas = document.querySelectorAll("div.uk-slider").innerHTML = function() {
   for (var i = 0; i < columna.length; i++) {
     columnas.style =
   }
 };
+
+//modificar el click en la lupa
+.onClick = function buscadorsecundario() {
+  var search = document.querySelector("button.fa fas-search");
+  if (search.style.display === "none") {
+    search.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+//hasta aca.
+}
