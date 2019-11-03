3efb72e9358688e311d36dd149640f9c0e4aff48
ariel <afarji@udesa.edu.ar>
2019-11-03 11:53:20 -0300

    Estilo y js de busqueda avanzada.
    Le agregue los titulos a las opciones de filtro y js a la primera opcion de generos

--- busquedaAvanzada.js
+++ busquedaAvanzada.js
@@ -1,4 +1,14 @@
-window.onload = function () {
+window.addEventListener("load", function () {
 
-  
-}
+  fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=a6f60714320c532cb6f1c6ddeef46bac&language=en-US")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(respuesta){
+    // console.log( respuesta.results[1].poster_path );
+    var series = respuesta.genres;
+    for (var i = 0; i < series.length; i++) {
+      document.querySelector("select.opcionesGenero").innerHTML += "<option>" + series[i].name + "</option>"
+    }
+  })
+})
