37fa1c3421452677158534def7364a3de5d55d60
ariel <afarji@udesa.edu.ar>
2019-11-05 12:30:54 -0300

    html y js de generos y resultado de genero y logo en footer
    Falta terminar de arreglar el js para que al clickear el genero mande al resultado de ese genero

--- generosxserie.js
+++ generosxserie.js
@@ -1,4 +1,7 @@
 window.addEventListener("load", function () {
+var idGenero = new URLSearchParams(location.search).get("idGenero")
+//var genero = idGenero.get("");ESTA VAR ME TIENE QUE DAR LA URL DE LAS SERIES
+
 
   fetch("https://api.themoviedb.org/3/genre/tv/list?api_key=a6f60714320c532cb6f1c6ddeef46bac&language=en-US")
   .then(function(response) {
@@ -8,7 +11,8 @@ window.addEventListener("load", function () {
     // console.log( respuesta.results[1].poster_path );
     var series = respuesta.genres;
     for (var i = 0; i < series.length; i++) {
-      document.querySelector("ul.lista-generos").innerHTML += "<li><a href=>" + series[i].name + "</a></li>"
+      document.querySelector("ul.lista-generos").innerHTML += "<li><a href=generosxserie.html?idGenero=" + series[i].id + ">" + series[i].name + "</a></li>"
     }
   })
+
 })
