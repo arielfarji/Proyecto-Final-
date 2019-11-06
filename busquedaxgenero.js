889e49fe28ae5c3f1be1fbfb8ddf5a192852f851
ariel <afarji@udesa.edu.ar>
2019-11-06 13:40:38 -0300

    Deje un comentario en el busquedaxgenero.js

--- busquedaxgenero.js
+++ busquedaxgenero.js
@@ -4,10 +4,11 @@ window.addEventListener("load", function (){
   var genero = idGenero.get("idGenero");
   //console.log(genero);
 
-   fetch("https://api.themoviedb.org/3/genre/tv/list" + idGenero + "?api_key=a6f60714320c532cb6f1c6ddeef46bac&language=en-US")
+   fetch("https://api.themoviedb.org/3/genre/tv/list" + idGenero + "?language=en-US&api_key=a6f60714320c532cb6f1c6ddeef46bac")
   .then(function(response) {
     return response.json();
   })
+
 })
 
 var lupita = document.querySelector("#lupita");
@@ -20,7 +21,7 @@ inputBuscador.classList.toggle('inputHidden');
 
 // Esto debiera ir despues del fetch
 // .then(function(respuesta) {
-//   document.querySelector("h1.title").innerText = "Resultado de ''" + respuesta.name + "''";
+//   document.querySelector("h1.title").innerText = "Resultado de "" + series[i].name; + """
 //   var series = respuesta.results;
 //   for (var i = 0; i < series.length; i++) {
 //     if(series[i].poster_path == null) {
