4ec3b26b34d1ed8cb1fb9c31df31b61585d38cd2
ariel <afarji@udesa.edu.ar>
2019-11-06 15:29:56 -0300

    js de generos terminado y funcionando

--- busquedaxgenero.js
+++ busquedaxgenero.js
@@ -2,23 +2,24 @@ window.addEventListener("load", function (){
 
   var idGenero = new URLSearchParams(location.search)
   var genero = idGenero.get("idGenero");
-  console.log(genero);
+  // console.log(genero);
 
-   fetch(" https://api.themoviedb.org/3/discover/tv?api_key=a6f60714320c532cb6f1c6ddeef46bac&sort_by=popularity.desc&page=1&with_genres=" + idGenero)
+   fetch(" https://api.themoviedb.org/3/discover/tv?api_key=a6f60714320c532cb6f1c6ddeef46bac&sort_by=popularity.desc&page=1&with_genres=" + genero)
   .then(function(response) {
     return response.json();
   })
-  // .then(function(respuesta) {
-  //   document.querySelector("h1.title").innerText = "Resultados"
-  //   var series = respuesta.results;
-  //   for (var i = 0; i < series.length; i++) {
-  //     if(series[i].poster_path == null) {
-  //       document.querySelector("div#busqueda").innerHTML += "<div class='pelis'id='errores'><img src='img/newError.jpeg'></div>";
-  //     } else {
-  //       document.querySelector("div#busqueda").innerHTML += "<div class='pelis'><img src='http://image.tmdb.org/t/p/w300" + series[i].poster_path + "'></div>";
-  //     }
-  //   }
-  // })
+  .then(function(respuesta) {
+    document.querySelector("h1.title").innerText = "Resultados"
+    var series = respuesta.results;
+    for (var i = 0; i < series.length; i++) {
+      if(series[i].poster_path == null) {
+        document.querySelector("div#busqueda").innerHTML += "<div class='pelis'id='errores'><img src='img/newError.jpeg'></div>";
+      } else {
+        document.querySelector("div#busqueda").innerHTML += "<div class='pelis'><img src='http://image.tmdb.org/t/p/w300" + series[i].poster_path + "'></div>";
+      }
+
+    }
+  })
 })
 
 var lupita = document.querySelector("#lupita");
