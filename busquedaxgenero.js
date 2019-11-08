f747df39b4f6dd112b5db6866b10eb550ee07ad9
ariel <afarji@udesa.edu.ar>
2019-11-08 14:26:46 -0300

    Solucionado for good

--- busquedaxgenero.js
+++ busquedaxgenero.js
@@ -49,7 +49,7 @@ setTimeout(function() {inputBuscador.focus();}, 301)
 }
 
 //busqueda sea valida y con mas de 3 caracteres y desaparezca dps de 3s
-document.querySelector("form#busqueta").onsubmit = function (event) {
+document.querySelector("form#busqueda").onsubmit = function (event) {
 
   if(document.querySelector("input.buscadorsecundario").value.length < 3) {
 event.preventDefault();
@@ -62,7 +62,3 @@ setTimeout(function(){
 }, 3000)
   }
   }
-
-
-
-}
--- resultado.js
+++ resultado.js
@@ -41,7 +41,7 @@ document.querySelector ("div.noHay").innerHTML += "<p>No se ha encontrado result
   }
 
   //busqueda sea valida y con mas de 3 caracteres y desaparezca dps de 3s
-  document.querySelector("form#busqueta").onsubmit = function (event) {
+  document.querySelector("form#busqueda").onsubmit = function (event) {
 
     if(document.querySelector("input.buscadorsecundario").value.length < 3) {
   event.preventDefault();
