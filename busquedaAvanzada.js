c253d8e6b9a9f9ff546db130a6900dd43afa96c6
ariel <afarji@udesa.edu.ar>
2019-11-08 15:57:39 -0300

    Buscador avanzado y funcionando

--- busquedaAvanzada.js
+++ busquedaAvanzada.js
@@ -9,19 +9,57 @@ window.addEventListener("load", function () {
     var series = respuesta.genres;
     for (var i = 0; i < series.length; i++) {
       document.querySelector("select.opcionesGenero").innerHTML += "<option value=" + series[i].id + ">" + series[i].name + "</option>"
-    }
-  })
-  fetch("https://api.themoviedb.org/3/genre/tv/list?api_key=a6f60714320c532cb6f1c6ddeef46bac&language=en-US")
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(respuesta){
-    // console.log( respuesta.results[1].poster_path );
-    var series = respuesta.genres;
-    for (var i = 0; i < series.length; i++) {
-      document.querySelector("select.excluir").innerHTML += "<option>" + series[i].name + "</option>"
+      document.querySelector("select.excluir").innerHTML += "<option value=" + series[i].id + ">" + series[i].name + "</option>"
     }
   })
 
   //Agregar ordenar por y años.
+
+  var query = new URLSearchParams(location.search)
+
+
+  var genero = query.get('genero')
+  if(genero != null){
+    genero = '&with_genres=' + genero
+  }else{
+    genero = ""
+  }
+
+  var excluir = query.get('excluir')
+  if(excluir != null){
+    excluir = '&without_genres=' + excluir
+  }else{
+    excluir = ""
+  }
+  var orden = query.get('orden')
+
+  if(orden!= null){
+    orden = '&sort_by=' + orden
+  }else{
+    orden = ""
+  }
+
+  var year = query.get('year')
+  if(year != null){
+    year = '&first_air_date_year=' + year
+  }else{
+    year = ""
+  }
+
+  fetch("https://api.themoviedb.org/3/discover/tv?api_key=a6f60714320c532cb6f1c6ddeef46bac" + genero + excluir + orden + year)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(respuesta){
+    console.log(respuesta);
+    var series = respuesta.results;
+    document.querySelector("h2.title").innerText = "Resultado de la busqueda avanzada";
+    for (var i = 0; i < series.length; i++) {
+      if(series[i].poster_path != null) {
+        document.querySelector("div#busqueda").innerHTML += "<div class='pelis'><img src='http://image.tmdb.org/t/p/w300" + series[i].poster_path + "'></div>";
+      }
+    }
+  })
+
+
 })
--- resultado.js
+++ resultado.js
@@ -5,32 +5,32 @@ window.onload = function() {
 
   document.querySelector('input.buscadorsecundario').value = loBuscado
 
-  fetch("https://api.themoviedb.org/3/search/tv?api_key=a6f60714320c532cb6f1c6ddeef46bac&language=en-US&query="+ loBuscado +"&page=1")
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(respuesta) {
-    document.querySelector("h2.title").innerText = "Resultado de ''" + loBuscado + "''";
-    var series = respuesta.results;
-    console.log(series);
-    if (series != false) {
-      document.querySelector("h1.title").innerText = "Resultado de ''" + loBuscado + "''";
-      for (var i = 0; i < series.length; i++) {
-        if(series[i].poster_path == null) {
-          document.querySelector("div#busqueda").innerHTML += "<div class='pelis'id='errores'><img src='img/newError.jpeg'></div>";
-        } else {
-          document.querySelector("div#busqueda").innerHTML += "<div class='pelis'><img src='http://image.tmdb.org/t/p/w300" + series[i].poster_path + "'></div>";
-        }
-    }
-
-    } else {
-      console.log('entre')
-document.querySelector ("div.noHay").innerHTML += "<p>No se ha encontrado resultados</p>"
-      }
-  })
-  .catch(function(error) {
-    alert("Error, perdon, vuelva mas tarde")
-  })
+//   fetch("https://api.themoviedb.org/3/search/tv?api_key=a6f60714320c532cb6f1c6ddeef46bac&language=en-US&query="+ loBuscado +"&page=1")
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(respuesta) {
+//     document.querySelector("h2.title").innerText = "Resultado de ''" + loBuscado + "''";
+//     var series = respuesta.results;
+//     console.log(series);
+//     if (series != false) {
+//       document.querySelector("h1.title").innerText = "Resultado de ''" + loBuscado + "''";
+//       for (var i = 0; i < series.length; i++) {
+//         if(series[i].poster_path == null) {
+//           document.querySelector("div#busqueda").innerHTML += "<div class='pelis'id='errores'><img src='img/newError.jpeg'></div>";
+//         } else {
+//           document.querySelector("div#busqueda").innerHTML += "<div class='pelis'><img src='http://image.tmdb.org/t/p/w300" + series[i].poster_path + "'></div>";
+//         }
+//       }
+//
+//     } else {
+//       console.log('entre')
+// document.querySelector ("div.noHay").innerHTML += "<p>No se ha encontrado resultados</p>"
+//       }
+//   })
+//   .catch(function(error) {
+//     alert("Error, perdon, vuelva mas tarde")
+//   })
 
   var lupita = document.querySelector("#lupita");
   var inputBuscador = document.querySelector(".buscadorsecundario");
@@ -41,19 +41,19 @@ document.querySelector ("div.noHay").innerHTML += "<p>No se ha encontrado result
   }
 
   //busqueda sea valida y con mas de 3 caracteres y desaparezca dps de 3s
-  document.querySelector("form#busqueda").onsubmit = function (event) {
-
-    if(document.querySelector("input.buscadorsecundario").value.length < 3) {
-  event.preventDefault();
-    document.querySelector('.error').innerHTML += `<div class="uk-alert-danger notificacion" uk-alert>
-      <a class="uk-alert-close" uk-close></a>
-      <p>Al menos 3 letras.</p>
-  </div>`
-  setTimeout(function(){
-    document.querySelector('.notificacion').style.display = 'none'
-  }, 3000)
-    }
-  }
+  // document.querySelector("form#busqueda").onsubmit = function (event) {
+  //
+  //   if(document.querySelector("input.buscadorsecundario").value.length < 3) {
+  // event.preventDefault();
+  //   document.querySelector('.error').innerHTML += `<div class="uk-alert-danger notificacion" uk-alert>
+  //     <a class="uk-alert-close" uk-close></a>
+  //     <p>Al menos 3 letras.</p>
+  // </div>`
+  // setTimeout(function(){
+  //   document.querySelector('.notificacion').style.display = 'none'
+  // }, 3000)
+  //   }
+  // }
 
 
 
