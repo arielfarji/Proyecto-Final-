72efbd6e84ab4579a9aa1535ce48d646df33169d
Tiziana Borghetti <tborghetti@udesa.edu.ar>
2019-11-08 13:57:11 -0300

    Cree el detalle de cada uno y termine el buscador

--- resultado.js
+++ resultado.js
@@ -3,20 +3,30 @@ window.onload = function() {
   var loBuscado = datos.get("buscadorsecundario");
   // console.log(loBuscado);
 
+  document.querySelector('input.buscadorsecundario').value = loBuscado
+
   fetch("https://api.themoviedb.org/3/search/tv?api_key=a6f60714320c532cb6f1c6ddeef46bac&language=en-US&query="+ loBuscado +"&page=1")
   .then(function(response) {
     return response.json();
   })
   .then(function(respuesta) {
-    document.querySelector("h1.title").innerText = "Resultado de ''" + loBuscado + "''";
+
     var series = respuesta.results;
-    for (var i = 0; i < series.length; i++) {
-      if(series[i].poster_path == null) {
-        document.querySelector("div#busqueda").innerHTML += "<div class='pelis'id='errores'><img src='img/newError.jpeg'></div>";
-      } else {
-        document.querySelector("div#busqueda").innerHTML += "<div class='pelis'><img src='http://image.tmdb.org/t/p/w300" + series[i].poster_path + "'></div>";
-      }
+    console.log(series);
+    if (series != false) {
+      document.querySelector("h1.title").innerText = "Resultado de ''" + loBuscado + "''";
+      for (var i = 0; i < series.length; i++) {
+        if(series[i].poster_path == null) {
+          document.querySelector("div#busqueda").innerHTML += "<div class='pelis'id='errores'><img src='img/newError.jpeg'></div>";
+        } else {
+          document.querySelector("div#busqueda").innerHTML += "<div class='pelis'><img src='http://image.tmdb.org/t/p/w300" + series[i].poster_path + "'></div>";
+        }
     }
+
+    } else {
+      console.log('entre')
+document.querySelector ("div.noHay").innerHTML += "<p>No se ha encontrado resultados</p>"
+      }
   })
   .catch(function(error) {
     alert("Error, perdon, vuelva mas tarde")
@@ -29,4 +39,23 @@ window.onload = function() {
     inputBuscador.classList.toggle('inputHidden');
     setTimeout(function() {inputBuscador.focus();}, 301)
   }
+
+  //busqueda sea valida y con mas de 3 caracteres y desaparezca dps de 3s
+  document.querySelector("form#busqueta").onsubmit = function (event) {
+
+    if(document.querySelector("input.buscadorsecundario").value.length < 3) {
+  event.preventDefault();
+    document.querySelector('.error').innerHTML += `<div class="uk-alert-danger notificacion" uk-alert>
+      <a class="uk-alert-close" uk-close></a>
+      <p>Al menos 3 letras.</p>
+  </div>`
+  setTimeout(function(){
+    document.querySelector('.notificacion').style.display = 'none'
+  }, 3000)
+    }
+  }
+
+
+
+
 }
