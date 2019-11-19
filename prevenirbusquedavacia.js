d1bdf111d42997c42430dce0079dcd68ac67d10b
ariel <afarji@udesa.edu.ar>
2019-11-19 15:25:05 -0300

    Restricciones busqueda avanzada y todas las columnas misma altura

--- prevenirbusquedavacia.js
+++ prevenirbusquedavacia.js
@@ -1,10 +1,33 @@
 window.addEventListener("load", function () {
 
-  document.querySelector(".form-buscador button").addEventListener("click", function (event) {
-    event.preventDefault()
-    // 1) capturo todos los campos del formulario
-    // 2) Valido que x lo menos un campo este completo
-    // 3) Si esto sucede lo dejo submitir, sino alerto que tiene que buscar algo al usuario
-    
-  })
+
+  document.querySelector(".form-avanzado").onsubmit = function (event) {
+    event.preventDefault();
+    var eGenero = document.querySelector(".opcionesGenero");
+    var generoElegido = eGenero.options[eGenero.selectedIndex].value;
+
+    var excluir = document.querySelector(".excluir");
+    var generoExcluido = excluir.options[excluir.selectedIndex].value;
+
+    var ordenar = document.querySelector(".orden");
+    var orden = ordenar.options[ordenar.selectedIndex].value;
+
+    var year = document.querySelector(".year").value;
+
+    event.preventDefault();
+    console.log(generoElegido);
+
+
+    if (generoElegido == "" && generoExcluido == "" && orden == "" && year == "" ) {
+      event.preventDefault();
+      alert('Seleccione al menos un campo')
+    }
+
+    if (generoElegido != "" && generoExcluido != "") {
+      event.preventDefault();
+      alert("tesdt")
+      // Mostrar otro error
+    }
+
+  }
 })
